fdb50eb7b83f6b3fd8b1b3ee62e72ce1077f9179
Anupreet Paulkar <anupreet@Anupreets-MacBook-Pro.local>
2023-08-28 21:35:24 -0500

    List updating

--- src/main/java/com/anupreet/studentmanagementspringbootrestfulapi/StudentManagementSpringBootResTfulApi.java
+++ src/main/java/com/anupreet/studentmanagementspringbootrestfulapi/StudentManagementSpringBootResTfulApi.java
@@ -1,5 +1,9 @@
 package com.anupreet.studentmanagementspringbootrestfulapi;
 
+import com.anupreet.studentmanagementspringbootrestfulapi.entity.Student;
+import com.anupreet.studentmanagementspringbootrestfulapi.repository.StudentRepository;
+import com.anupreet.studentmanagementspringbootrestfulapi.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +17,18 @@ public class StudentManagementSpringBootResTfulApi implements CommandLineRunner 
 		System.out.println("Hello there");
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
 	@Override
 	public void run(String... args) throws Exception {
 
+		Student student1 = new Student("Anu", "Paulkar", "anu@email.com");
+		studentRepository.save(student1);
+
+		Student student2 = new Student("Sam", "Bhagat", "sam@email.com");
+		studentRepository.save(student2);
+
+		Student student3 = new Student("Sam", "Paulkar", "sam@easdadmail.com");
+		studentRepository.save(student3);
 	}
 }
